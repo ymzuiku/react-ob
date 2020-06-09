35294f0971001937ef72d25235a410050beb242b
梁柱 <ymblender@gmail.com>
2020-06-09 17:35:13 +0800

    fix: remove immer

--- lib/index.ts
+++ lib/index.ts
@@ -1,15 +1,31 @@
 import { useState, useEffect } from "react";
 import TinySubject from "tiny-subject";
-import immer from "immer";
 
-export default function reactOb<T, A>(initState: T, fn?: A) {
+export interface UseOBOptions<A> {
+  actions?: A;
+  immer?: any;
+}
+
+export default function reactOb<T, A>(
+  initState: T,
+  options: UseOBOptions<A> = {}
+) {
+  const { actions = {}, immer } = options;
   const subject = new TinySubject(initState);
   type Updater = (s: T) => any;
 
-  function useOb(memo?: (s: T) => any[]): T {
+  function useOb(memo?: (s: T) => any[], autoFn?: Function[]): T {
     const [state, setState] = useState(subject.state);
 
     useEffect(() => {
+      if (autoFn) {
+        autoFn.forEach(async (fn) => {
+          const backFn = await Promise.resolve(fn);
+          if (typeof backFn === "function") {
+            backFn();
+          }
+        });
+      }
       let unsub: any;
 
       if (memo) {
@@ -36,18 +52,23 @@ export default function reactOb<T, A>(initState: T, fn?: A) {
     return JSON.parse(JSON.stringify(baseState));
   };
 
-  useOb.State = initState;
+  useOb.get = () => subject.state;
 
   useOb.set = function (fn: Updater) {
-    subject.state = immer<T>(subject.state, (draft) => {
-      fn(draft as any);
-    });
+    if (immer) {
+      subject.state = immer(subject.state, (draft: T) => {
+        fn(draft as any);
+      });
+    } else {
+      fn(subject.state);
+      subject.state = { ...subject.state };
+    }
+
     subject.next();
   };
+  useOb.next = () => useOb.set(() => {});
 
-  useOb.fn = fn;
+  useOb.fn = actions as A;
 
   return useOb;
 }
-
-export type UseStoreFetcher<T> = (p: any, s: T) => Promise<T>;
--- umd/index.d.ts
+++ umd/index.d.ts
@@ -1,8 +1,12 @@
-export default function reactOb<T, A>(initState: T, fn?: A): {
-    (memo?: ((s: T) => any[]) | undefined): T;
+export interface UseOBOptions<A> {
+    actions?: A;
+    immer?: any;
+}
+export default function reactOb<T, A>(initState: T, options?: UseOBOptions<A>): {
+    (memo?: ((s: T) => any[]) | undefined, autoFn?: Function[] | undefined): T;
     getBaseState(): T;
-    State: T;
+    get(): T;
     set(fn: (s: T) => any): void;
-    fn: A | undefined;
+    next(): void;
+    fn: A;
 };
-export declare type UseStoreFetcher<T> = (p: any, s: T) => Promise<T>;
